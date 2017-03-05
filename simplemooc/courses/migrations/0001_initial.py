b29f2ce66b9da4f3ef92f5d06af76ad8289b4932
Leonardo Rosa <leoamorimrosa@gmail.com>
2017-03-05 11:55:32 -0300

    initial commit

---
+++ simplemooc/courses/migrations/0001_initial.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='Course',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', primary_key=True, serialize=False, auto_created=True)),
+                ('name', models.CharField(max_length=100, verbose_name='Nome')),
+                ('slug', models.SlugField(verbose_name='Atalho')),
+                ('description', models.TextField(blank=True, verbose_name='Descrição')),
+                ('start_date', models.DateField(blank=True, null=True, verbose_name='Data de Início')),
+                ('image', models.ImageField(verbose_name='Imagem', upload_to='courses/images')),
+                ('created_at', models.DateTimeField(auto_now_add=True, verbose_name='Criado em')),
+                ('updated_at', models.DateTimeField(auto_now=True, verbose_name='Atualizado_em')),
+            ],
+        ),
+    ]
